ee1f60a962f7711a2ecc6a98bf3e896c48e08cd2
EdizCinbas <oecinbas@gmail.com>
2024-06-04 22:10:30 +0100

    Checkpoint: Security and account login

--- src/main/java/dev/ediz/maple/controller/RegisterController.java
+++ src/main/java/dev/ediz/maple/controller/RegisterController.java
@@ -1,4 +1,30 @@
 package dev.ediz.maple.controller;
 
+
+import dev.ediz.maple.model.Account;
+import dev.ediz.maple.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class RegisterController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute Account account) {
+        accountService.save(account);
+
+        return "redirect:/";
+    }
+
 }
