f434ec97315f85e2807c4df14cd008ae495fda65
EdizCinbas <oecinbas@gmail.com>
2026-03-07 12:37:07 +0000

    Changes to seed data

--- src/main/java/dev/ediz/maple/config/SeedData.java
+++ src/main/java/dev/ediz/maple/config/SeedData.java
@@ -2,23 +2,34 @@ package dev.ediz.maple.config;
 
 import dev.ediz.maple.model.Account;
 import dev.ediz.maple.model.Authority;
-import dev.ediz.maple.model.Post;
+import dev.ediz.maple.repository.AccountRepository;
 import dev.ediz.maple.repository.AuthorityRepository;
 import dev.ediz.maple.service.AccountService;
-import dev.ediz.maple.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Component
 public class SeedData implements CommandLineRunner {
 
+    @Value("${app.seed.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.seed.admin.password}")
+    private String adminPassword;
+
+    @Value("${app.seed.admin.firstName}")
+    private String adminFirstName;
+
+    @Value("${app.seed.admin.lastName}")
+    private String adminLastName;
+
     @Autowired
-    private PostService postService;
+    private AccountRepository accountRepository;
 
     @Autowired
     private AccountService accountService;
@@ -27,43 +38,30 @@ public class SeedData implements CommandLineRunner {
     private AuthorityRepository authorityRepository;
 
     @Override
-    public void run(String... args) throws Exception {
-        List<Post> posts = postService.getAll();
+    public void run(String... args) {
+        // Only runs if no admin account exists - ensures the app is always operable
+        if (accountRepository.existsAdminAccount()) return;
 
-        if (posts.isEmpty()) {
+        Authority roleUser = new Authority();
+        roleUser.setName("ROLE_USER");
+        authorityRepository.save(roleUser);
 
-            Authority user = new Authority();
-            user.setName("ROLE_USER");
-            authorityRepository.save(user);
+        Authority roleAdmin = new Authority();
+        roleAdmin.setName("ROLE_ADMIN");
+        authorityRepository.save(roleAdmin);
 
-            Authority admin = new Authority();
-            admin.setName("ROLE_ADMIN");
-            authorityRepository.save(admin);
+        Account account = new Account();
+        account.setFirstName(adminFirstName);
+        account.setLastName(adminLastName);
+        account.setEmail(adminEmail);
+        account.setPassword(adminPassword);
+        account.setEnabled(true);
 
-            Account account = new Account();
-            account.setFirstName("admin");
-            account.setLastName("admin");
-            account.setEmail("admin.admin@domain.com");
-            account.setPassword("password");
-            account.setEnabled(true);
-            Set<Authority> authorities = new HashSet<>();
-            authorityRepository.findById("ROLE_USER").ifPresent(authorities::add);
-            authorityRepository.findById("ROLE_ADMIN").ifPresent(authorities::add);
-            account.setAuthorities(authorities);
-            accountService.save(account);
+        Set<Authority> authorities = new HashSet<>();
+        authorityRepository.findById("ROLE_USER").ifPresent(authorities::add);
+        authorityRepository.findById("ROLE_ADMIN").ifPresent(authorities::add);
+        account.setAuthorities(authorities);
 
-            Post post1 = new Post();
-            post1.setTitle("Title of post 1");
-            post1.setBody("Body of post 1");
-            post1.setAccount(account);
-
-            Post post2 = new Post();
-            post2.setTitle("Title of post 2");
-            post2.setBody("Body of post 2");
-            post2.setAccount(account);
-
-            postService.save(post1);
-            postService.save(post2);
-        }
+        accountService.save(account);
     }
 }
--- src/main/java/dev/ediz/maple/repository/AccountRepository.java
+++ src/main/java/dev/ediz/maple/repository/AccountRepository.java
@@ -2,11 +2,15 @@ package dev.ediz.maple.repository;
 
 import dev.ediz.maple.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findOneByEmail (String email);
+    Optional<Account> findOneByEmail(String email);
+
+    @Query("SELECT COUNT(a) > 0 FROM Account a JOIN a.authorities auth WHERE auth.name = 'ROLE_ADMIN'")
+    boolean existsAdminAccount();
 }
